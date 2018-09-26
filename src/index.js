decca804fff5fdb4c50e57cc0bad35084a16e138
megharana <megharanait95@gmail.com>
2018-09-26 22:38:53 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -7,13 +7,44 @@ var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class')
 
 var ToDoComponent = createReactClass({
+	getInitialState:function(){
+		return{
+			todos:['wash up','eat some cheese','take a nap']
+		}
+	},
+	render:function(){
+		var todos = this.state.todos;
+		todos = todos.map(function(item,index){
+			return(
+				
+					<ToDoItem item={item} key={index}/>
+				
+			);
+		});
+		
+		return(
+			<div id="todo-list">
+				<h1>Megha</h1>
+				
+				
+
+				<ul>{todos}</ul>
+			</div>
+
+		);
+	} 
+}); 
+
+//create ToDoItem 
+
+var ToDoItem = createReactClass({
 	render:function(){
 		return(
-			<div>
-				<h1>Megha</h1>
-				<p>{this.props.mssg}</p>
-				<p><strong>{this.props.cheese.name}</strong></p>
-			</div>
+			<li>
+				<div className = "todo-item">
+					<span className="item-name">this.props.item</span>
+				</div>
+			</li>
 		);
 	}
 });
